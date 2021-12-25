ad0ae13a70f3b827bd2a57349fa574cb592fde42
Myltiane Alves <myltiane.aux@gmail.com>
2021-12-25 11:19:33 -0300

    Finalizando

--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,18 +1,29 @@
 import * as C from './styles';
 import { Item } from '../../types/item';
-import { useState } from 'react';
+import { useState, KeyboardEvent } from 'react';
 
 type Props = {
-    item: Item
+    onEnter: (taskName: string) => void;
 }
-export const AddArea = () => {
+export const AddArea = ({ onEnter }: Props) => {
+    const [inputText, setInputText] = useState('');
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if(e.code === 'Enter' && inputText !== '') {
+            onEnter(inputText);
+            setInputText('');
+        }
+    }
 
     return(
         <C.Container >
             <div className="image">➕</div>
             <input 
                 type="text"
-            
+                placeholder="Adicione uma tarefa"
+                value={inputText}
+                onChange={e=>setInputText(e.target.value)}
+                onKeyUp={handleKeyUp}
             />
             
         </C.Container>
